e3347b0b612abd69124dd5987a6147ccf9462263
Akash K <akashkuruba6@gmail.com>
2026-08-09 11:49:29 +0530

    Version 2 - Constructors and this Keyword

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,15 @@ public class Main {
 
     public static void main (String[] args) {
 
-        Student s1 = new Student();
+        Student s1 = new Student("Akash",21,"IT","24BBTIT004");
 
-        s1.name = "Akash";
-        s1.age = 21;
-        s1.branch = "IT";
-        s1.usn = "24BBTIT004";
+        Student s2 = new Student("Ranjith");
 
         System.out.println(s1.name);
         System.out.println(s1.age);
         System.out.println(s1.branch);
         System.out.println(s1.usn);
 
+        System.out.println(s2.name);
     }
-
 }
--- src/Student.java
+++ src/Student.java
@@ -5,4 +5,20 @@ public class Student {
     String branch;
     String usn;
 
+    Student() {
+
+    }
+
+    Student (String name, int age, String branch, String usn) {
+
+        this.name = name;
+        this.age = age;
+        this.branch = branch;
+        this.usn = usn;
+    }
+
+    Student(String name) {
+
+        this.name = name;
+    }
 }
